a9d6b3b59231a32769412a9e497920a84545ea35
Justin Guenther <jguenther@vendasta.com>
2016-10-05 09:17:44 -0600

    Use `sleep-for` instead of `sit-for`

--- org-protocol-capture-html.el
+++ org-protocol-capture-html.el
@@ -33,7 +33,7 @@
       (while (process-live-p process)
         (if (= checked limit)
             (error "Unable to test Pandoc!  Please report this bug! (include the output of \"pandoc --dump-args --no-wrap\")")
-          (sit-for 0.2)
+          (sleep-for 0.2)
           (incf checked)))
       (if (and (= 0 (process-exit-status process))
                (not (string-match "--no-wrap is deprecated" (buffer-string))))
